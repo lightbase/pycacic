967720a8dee212760cdf0b87338d371a5bd88a9f
rogeriolino@gmail.com <rogeriolino@gmail.com>
2008-05-08 16:12:11 +0000

    Removido utilizacao de pacote extra egg para gerar Status Icon.
    Rogério A. Lino Filho
    
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/agente-linux/pycacic@426 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/gui.py
+++ src/gui.py
@@ -22,32 +22,62 @@ class GUI:
     
     def __init__(self):
         self.createTray()
-        self.mw = MainWindow()
+        self.mw = MainWindow()        
     
     def createTray(self):
-        # TrayIcon
-        self.tray = TrayIcon('pycacic')
-        self.eventbox = gtk.EventBox()
-        self.tray.add(self.eventbox)
-        self.eventbox.connect("button_press_event", self.tray_icon_clicked)
-        # Create the tooltip for the tray icon
-        self.tooltip = gtk.Tooltips()
-        # Configura imagem para o tray icon
+        # TrayIcon        
+        self.statusIcon = gtk.StatusIcon()
+        
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("StatusIcon test (II)")
+        self.window.connect('delete_event', self.delete_cb, self.statusIcon)
+ 
+        self.menu = gtk.Menu()
+        self.menuItem = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
+        self.menuItem.connect('activate', self.activate_icon_cb)
+        self.menu.append(self.menuItem)
+        self.menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+        self.menuItem.connect('activate', self.quit_cb, self.statusIcon)
+        self.menu.append(self.menuItem)
+        
         self.imageicon = gtk.Image()
         pixbuf = gtk.gdk.pixbuf_new_from_file(self.ICON_PATH)
         scaled_buf = pixbuf.scale_simple(23, 23, gtk.gdk.INTERP_BILINEAR)
-        self.imageicon.set_from_pixbuf(scaled_buf)
-        self.eventbox.add(self.imageicon)
-        # Exibe o tray icon
-        self.tray.show_all()
-        self.tooltip.set_tip(self.tray,'pycacic')
+        self.statusIcon.set_from_pixbuf(scaled_buf)
+        
+        self.statusIcon.set_tooltip("Pycacic")
+        self.statusIcon.connect('activate', self.activate_icon_cb)
+        self.statusIcon.connect('popup-menu', self.popup_menu_cb, self.menu)
+        self.statusIcon.set_visible(True)
     
-    def tray_icon_clicked(self, signal, event):
-        """ """
-        if event.button == 3:
-            PopupMenu().show_menu(event)
-        else:
-            self.mw.toggleWindow()
+    def delete_cb(widget, event, data = None):
+        if data:
+            data.set_blinking(True)
+        return False
+        
+    def quit_cb(self, widget, data = None):
+        if data:
+            dialog = gtk.MessageDialog( None, gtk.DIALOG_MODAL, gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO, 'Deseja realmente sair?')        
+            dialog.set_title('PyCacic')                
+            dialog.set_icon_from_file(self.ICON_PATH)
+            dialog.width, dialog.height = dialog.get_size()
+            dialog.move(gtk.gdk.screen_width()/2-dialog.width/2, gtk.gdk.screen_height()/2-dialog.height/2)
+            retorno = dialog.run()              
+            if retorno == gtk.RESPONSE_YES:    
+                self.quit()
+            dialog.destroy()
+            data.set_visible(False)
+    
+        
+    def activate_icon_cb(self, widget, data = None):
+        self.mw.toggleWindow()    
+    
+    def popup_menu_cb(self, widget, button, time, data = None):
+        if button == 3:
+            if data:
+                data.show_all()
+                data.popup(None, None, None, 3, time)
+
 
     def quit(self):
         """Fecha o programa"""
@@ -103,46 +133,6 @@ class MainWindow(GUI):
         if LOGIN_ROOT != '':
             self.root_login_xml.get_widget("root_login").destroy()
             
-
-class PopupMenu(GUI):
-    
-    def __init__(self):     
-        # Create menu items
-        self.item_lang = gtk.MenuItem('Idioma', True)
-        self.item_about = gtk.MenuItem('Sobre', True)
-        self.item_exit = gtk.MenuItem('Sair', True)
-        # Connect the events
-        self.item_lang.connect('activate', self.conflang)
-        self.item_about.connect('activate', self.about)                           
-        self.item_exit.connect('activate', self.exit)
-        # Create the menu
-        self.menu = gtk.Menu()
-        # Append menu items to the menu
-        self.menu.append(self.item_lang)
-        self.menu.append(self.item_about)        
-        self.menu.append( self.item_exit)
-        self.menu.show_all()
-
-    def show_menu(self, event):
-        # Display the menu
-        self.menu.popup(None, None, None, event.button, event.time)
-    
-    def conflang(self, event):
-        pass
-    
-    def about(self, event):
-        pass               
-    
-    def exit(self, event):
-        dialog = gtk.MessageDialog( None, gtk.DIALOG_MODAL, gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO, 'Deseja realmente sair?')        
-        dialog.set_title('PyCacic')                
-        dialog.set_icon_from_file(self.ICON_PATH)
-        dialog.width, dialog.height = dialog.get_size()
-        dialog.move(gtk.gdk.screen_width()/2-dialog.width/2, gtk.gdk.screen_height()/2-dialog.height/2)
-        retorno = dialog.run()              
-        if retorno == gtk.RESPONSE_YES:            
-            self.quit()                        
-        dialog.destroy()
 
 if __name__ == '__main__':
     try:
