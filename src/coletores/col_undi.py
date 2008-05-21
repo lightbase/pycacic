307e54991e14817cf0052e6fec0c87344958ecc1
rogeriolino@gmail.com <rogeriolino@gmail.com>
2008-05-21 17:25:45 +0000

    Criado coletor de unidade de discos (Col_Undi)
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/agente-linux/pycacic@453 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ src/coletores/col_undi.py
@@ -0,0 +1,42 @@
+
+from coletor import *
+from globals import Globals
+
+class Col_Undi(Coletor):
+    """Classe responsavel por coletar os dados das Unidades de Disco"""
+    
+    # nome do arquivo de saida (DAT)
+    OUTPUT_DAT = 'col_undi.dat'
+
+    def __init__(self, computer):
+        Coletor.__init__(self, computer)
+        self.computer = computer
+        
+    def getName(self):
+        return "col_undi"
+    
+    def getUVCKey(self):
+        return 'Coleta.Undi'
+
+    def isReady(self, dat=None):
+        return self.getUVCDat(dat, self.getUVCKey()) != self.getChave('UVC')    
+
+    def setDicionario(self):
+        """Monta o dicionario"""        
+        self.dicionario.clear()
+        self.addChave('Inicio', '')
+        parts = self.computer.getPartitions()
+        """
+            Drive <FIELD>
+            id_tipo_unid_disco <FIELD>
+            FileSystem <FIELD>
+            SerialNumber <FIELD>
+            Capacity <FIELD>
+            FreeSpace <FIELD>
+            ???? <FIELD>
+        """
+        tripa = '<REG>'.join(['<FIELD>'.join([p.getName(), '2', p.getFileSystem(), p.getSerial(), str(p.getSize()), str(p.getFreeSize()), '']) for  p in parts ])
+        self.addChave('UnidadesDiscos', tripa)
+        self.addChave('Fim', '')
+        self.addChave('UVC', self.getUVC(self.dicionario))
+        
