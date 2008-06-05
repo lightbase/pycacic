51e2fa657d4d73ed5ff455f61201d41bd8f4797a
rogeriolino@gmail.com <rogeriolino@gmail.com>
2008-06-05 15:17:55 +0000

    Conclusao do módulo de tradução.
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/agente-linux/pycacic@473 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/lang/language.py
+++ src/lang/language.py
@@ -5,77 +5,100 @@ import sys
 import codecs
 from xml.dom import minidom, Node
 
+from logs.log import CLog
 from globals import Globals
 
 class Language:
     """Classe que controla o idioma do aplicativo"""
     
-    # xml infos
-    version = ''
-    author = ''
-    date = ''
-    language = ''
-    description = ''
-    
-    # Supported Languages Dictonary
-    langs = {
-             'pt_BR' : 'pt_br.xml', # (Portuguese - Brasil)
-             'pt_PT' : 'pt_pt.xml', # (Portuguese - Portugal)
-             'en_US' : 'en_us.xml', # (English - USA)
-             #'es_ES' : 'es_ES.xml', # (Spanish - Spain)
-             #'es_ AR' : 'es_ar.xml' # (Spanish - Argentina)
-             #'es_ MX' : 'es_mx.xml' # (Spanish - Mexico)
-             #'fr_FR' : 'fr_fr.xml', # (French - France)
-             #'fr_CA' : 'fr_fr.xml', # (French - Canada)
-             #'de_ DE' : 'de_de.xml', # (German - Germany)
-    }
     # default language
     DEFAULT = 'en_US'
     
-    
-    def __init__(self, mode = 'text'):
+    def __init__(self):
         """Construtor da classe"""
-        self.setMode(mode)
+        self.dict = {}
+        self.languages = []
+        self.scanAvailableLanguages()
         self.lang = self.getSOLang()
-        self.openXML()
-        self.setXMLInfo()
+        self.setActiveLanguageDict(self.openXML(self.lang))
         
+    def scanAvailableLanguages(self):
+        list = os.listdir('%s/lang/' % Globals.PATH)
+        for file in list:
+            if file.endswith(".xml"):
+                try:
+                    langInfo = self.getXMLHeader(file)
+                    self.languages.append(langInfo.getCode().lower())
+                except Exception, e:
+                    # log error
+                    CLog.appendLine('Language', e)
     
-    def openXML(self):
+    def addLanguage(self, langInfo):
+        Language.languages[langInfo.getCode().lower()] = langInfo;
+        
+    def getLanguageByCode(self, code):
+        try:
+            return Language.languages[code.lower()]
+        except:
+            return None
+    
+    def getXMLHeader(self, file):
+        xml = minidom.parse(Globals.PATH + '/lang/' + file)
+        try:
+            return self.parseLanguageInfo(file, xml)
+        except Exception, e:
+            raise Exception("Failed to load: lang/"+file+" - Reason: %s" % e)        
+        
+    def parseLanguageInfo(self, file, xml):
+        root = self.getRoot(xml)
+        for child in root.childNodes:
+            if child.nodeType == Node.ELEMENT_NODE and child.nodeName == "header":
+                code = ''
+                name = ''
+                for c in child.childNodes:
+                    if c.nodeType == Node.ELEMENT_NODE:
+                        if c.nodeName == "code":
+                            code = c.firstChild.nodeValue
+                        elif c.nodeName == "name":
+                            name = c.firstChild.nodeValue
+                if code != '' and name != '':
+                    return LanguageInfo(file, code, name)
+        raise Exception("Language code and/or name missing/empty.")
+    
+    def getLanguageInfoByCode(self, code):
+        for lang in self.languages:
+            if lang.getCode() == code:
+                return lang
+        return None
+    
+    def loadLanguageByCode(self, code):
+        langInfo = Language.getLanguageInfoByCode(code)
+        if langInfo != None:
+            langInfo.load()
+    
+    def loadFromFile(self, file):
+        pass
+    
+    def setActiveLanguageDict(self, xml):
+        root = self.getRoot(xml)
+        body = root.getElementsByTagName('body')[0]
+        for cat in body.childNodes:
+            if cat.nodeType == Node.ELEMENT_NODE:
+                self.dict[cat.nodeName] = {}
+                for item in cat.childNodes:
+                    if item.nodeType == Node.ELEMENT_NODE and item.nodeName == 'string':
+                        self.dict[cat.nodeName][item.attributes.get('name').nodeValue] = item.firstChild.nodeValue           
+    
+    def openXML(self, lang):
         """Abre o arquivo XML contendo o idioma escolhido"""
-        xml_file = open(Globals.PATH + '/lang/' + self.langs[self.lang], 'r').read()
-        self.xml = minidom.parseString(xml_file)
-    
-    
-    def setXMLInfo(self):
-        """Set XML info in the class vars"""
-        root = self.getRoot()
-        about = root.getElementsByTagName('about')[0]
-        for info in about.childNodes:
-            if info.nodeType == Node.ELEMENT_NODE:
-                if info.nodeName == 'version':
-                    self.version = info.firstChild.nodeValue
-                if info.nodeName == 'author':
-                    self.author = info.firstChild.nodeValue
-                if info.nodeName == 'date':
-                    self.date = info.firstChild.nodeValue
-                if info.nodeName == 'language':
-                    self.language = info.firstChild.nodeValue    
-                if info.nodeName == 'description':
-                    self.description = info.firstChild.nodeValue
-    
-    
-    def setMode(self, mode):
-        """
-            Seta o modo de exibicao do programa (texto ou grafico)
-            para pegar a respectiva traducao
-        """
-        if not mode in ('text', 'gui'):
-            self.mode = 'text'
-        else:
-            self.mode = mode
-    
-    
+        path = '%s/lang/%s.xml' % (Globals.PATH, self.lang.lower())
+        try:
+            xml_file = codecs.open(path, "r", "utf-8")
+            xml = u"%s" % xml_file.read()
+            return minidom.parseString(xml.encode("utf-8"))
+        except:
+            raise Exception("Error on open language file: %s" % path)
+       
     def chooseLang(self, lang):
         """
             Altera o idioma do aplicativo caso o mesmo esteja
@@ -86,8 +109,7 @@ class Language:
         else:
             self.lang = DEFAULT
         self.openXML()
-        self.setXMLInfo()
-        
+        self.setXMLInfo()        
     
     def getHeader(self):
         """Retorna o cabecalho do programa"""
@@ -100,70 +122,59 @@ class Language:
                 for item in filho.childNodes:
                     if item.nodeType == Node.ELEMENT_NODE:
                         header[item.nodeName] = item.firstChild.nodeValue
-        return header
-
-    
-    def getMenu(self):
-        """Retorna o menu principal do programa"""
-        no = self.getMode(self.mode)
-        if not no:
-            return 0 # False
-        menu = {}
-        for filho in no.childNodes:
-            if filho.nodeType == Node.ELEMENT_NODE and filho.nodeName == 'menu':
-                for item in filho.childNodes:
-                    if item.nodeType == Node.ELEMENT_NODE:
-                        menu[item.attributes.get('href').nodeValue] = item.firstChild.nodeValue
-        return menu
-    
-    
-    def getMessagesHitKeys(self):
-        """Retorna as mensagens de pressionar botao"""
-        no = self.getMessages()
-        if not no:
-            return 0 # False
-        messages = {}
-        for filho in no.childNodes:
-            if filho.nodeType == Node.ELEMENT_NODE and filho.nodeName == 'hitkeys':
-                for item in filho.childNodes:
-                    if item.nodeType == Node.ELEMENT_NODE:
-                        messages[item.attributes.get('key').nodeValue] = item.firstChild.nodeValue
-        return messages
-    
-    
-    def getMessages(self):
-        """Retorna todas a mensagens do programa"""
-        no = self.getMode(self.mode)
-        if not no:
-            return 0 # False
-        return no.getElementsByTagName('messages')[0]
-    
-    
-    def getMode(self, mode):
-        """
-            Retorna todas as informacoes do 
-            programa do modo especificado
-        """
-        if not mode in ('text', 'gui'):
-            return 0 # False
-        root = self.getRoot()
-        for no in root.childNodes:
-            if no.nodeType == Node.ELEMENT_NODE and no.nodeName == 'mode':
-                atributos = no.attributes
-                for a in atributos.keys():
-                    valor = atributos.get(a).nodeValue
-                    if a == 'type' and valor == mode:
-                        return no
+        return header   
                     
-    def getRoot(self):
+    def getRoot(self, xml):
         """Retorna o no raiz do XML"""        
-        root = self.xml.getElementsByTagName('pycacic')[0]
+        root = xml.getElementsByTagName('pycacic')[0]
         return root
-
     
     def getSOLang(self):
-        so_lang = os.environ['LANG']
-        for lang in self.langs:
-            if so_lang.find(lang.lower()) != -1:
+        so_lang = os.environ['LANG'].lower()
+        for lang in self.languages:
+            if so_lang.find(lang) != -1:
                 return lang
         return self.DEFAULT
+    
+    def get(self, name):
+        """"""
+        for item in self.dict.keys():
+            try:
+                return self.dict[item][name]
+            except:
+                pass
+        return 'Translation of "%s" not Found' % name    
+        
+    
+class LanguageInfo:
+    
+    def __init__(self, file, code, name):
+        self.file = file
+        self.code = code
+        self.name = name
+    
+    def getName(self):
+        return self.name
+    
+    def getCode(self):
+        return self.code
+    
+    def getFile(self):
+        return self.file
+    
+    def load(self):
+        Language.loadFromFile(self, self.getFile())     
+    
+
+class LanguageDictionary:
+    
+    def __init__(self, langInfo):
+        self.langInfo = langInfo
+        self.load()
+    
+    def load(self):
+        file = Globals.PATH + '/lang/' + langInfo.getFile()
+        xml = minidom.parse(file)
+        root = Language.getRoot(xml)
+        
+        
